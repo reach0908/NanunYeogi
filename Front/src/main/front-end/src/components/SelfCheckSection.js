cc1f1b69e5776ac618bdb71fe58a9dff850583b5
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-12-09 22:24:33 +0900

    add selfcheck function

--- Front/src/main/front-end/src/components/SelfCheckSection.js
+++ Front/src/main/front-end/src/components/SelfCheckSection.js
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../App.css';
-import {Button} from './Button';
-import {LoginButton} from './LoginButton';
 import './HeroSection.css';
+import axios from 'axios';
+import moment from 'moment'
 
 
-function SelfCheckSection() {
 
-    return (
-        <div className='self-container'>
-            <h1 className='title'>Self-Check(개발중)</h1>
-            <p>현재 코로나 거리두기 상태 : 2.5단계</p>
-            <p>2주간 사용자 외출 횟수 : 24회</p>
-            <p>일일 최대 시설 방문 횟수 : 5회</p>
-            <p className="self-alert">외출이 잦습니다. 코로나 감염예방을 위해 집콕을 추천드려요!</p>
+export default class SelfCheckSection extends Component {
+
+    state = {
+        startDate: new Date(),
+        recordList: [],
+    };
+
+    loadMyrecords = async () => {
+
+        const date = this.state.startDate;
+        const dateTime = moment(date).format("YYYY-MM-DD HH:mm:ss");
+        console.log(dateTime);
+        axios.get('http://nanserver.paas-ta.org/locations/' + window.localStorage.getItem("id"), {params: {date: dateTime}}).then((response) => {
+            this.setState(
+                {recordList: response.data}
+            )
+            console.log(this.state.recordList)
+        }).catch((error) => {
+            console.log(error);
+        });
+    };
+
+    componentDidMount() {
+        this.loadMyrecords();
+    }
+
+    render() {
+        const record = this.state.recordList;
+        
+        return (
+            <div>
+                {record.map(myrecord=>(
+                    <div className='self-container' key={myrecord.id}>
+                        <h1 className='title'>Self-Check</h1>
+                        <p>현재 코로나 거리두기 상태 : 2.5단계</p>
+                        <p>2주간 사용자 외출 횟수 : {myrecord.total}</p>
+                        <p>일일 최대 시설 방문 횟수 : {myrecord.today}</p>
+                        <p className="self-alert">외출이 잦습니다. 코로나 감염예방을 위해 집콕을 추천드려요!</p>
+                    </div>
+                ))}
+            
         </div>
-    )
+        );
+    }
 }
-
-export default SelfCheckSection
